ff507e42d65d14e39bc396aef837de157a8fab35
Jack <92jackhenderson@gmail.com>
2017-08-08 13:35:29 +0100

    Use webpack to create bundle that includes React
    - Install webpack
    - Create webpack.config.js and configure for babel-loader
    - Update package.json scripts
    - Import react library through webpack
    - Remove index.html CDN scripts

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'whatwg-fetch';
+
 const contentNode = document.getElementById('contents');
 const component = <h1> BATTLESHIP </h1>;
 ReactDOM.render(component, contentNode);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const buildDirectory = './static/';
+
+
+module.exports = {
+  entry: './src/App.jsx',
+  output: {
+    path: path.resolve(buildDirectory),
+    filename: 'app.bundle.js',
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react','es2015']
+        }
+      },
+    ]
+  },
+};
